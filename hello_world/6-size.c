34e7b19f9693bc16f8c46a67149bc0d932a1d615
Mathijs Dickson <mathijs.dickson@outlook.com>
2025-02-18 04:55:55 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,8 +10,8 @@ int main(void)
 	printf(Size of a char : %lu bytes \n, sizeof(char));
 	printf(Size of an int : %lu bytes \n, sizeof(int));
 	printf(Size of a long int : %lu bytes \n, sizeof(long int));
-	printf(Size of a long long int : %lu \n, sizeof(long int));
-	printf(Size of a float : %lu \n, sizeof(long int));
+	printf(Size of a long long int : %lli \n, sizeof(long long int));
+	printf(Size of a float : %f \n, sizeof(float));
      		
        	return (0);
 }
