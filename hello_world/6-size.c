d66655c77ece05f2c22f1cfc977c43f130315b99
Mathijs Dickson <mathijs.dickson@outlook.com>
2025-02-18 21:39:31 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	printf(Size of a char : %lu bytes \n, sizeof(char));
-	printf(Size of an int : %lu bytes \n, sizeof(int));
-	printf(Size of a long int : %lu bytes \n, sizeof(long int));
-	printf(Size of a long long int : %lli \n, sizeof(long long int));
-	printf(Size of a float : %f \n, sizeof(float));
-     		
-       	return (0);
+	printf("Size of a char : %lu bytes\n," sizeof(char));
+	printf("Size of an int : %lu bytes\n," sizeof(int));
+	printf("Size of a long int : %lu bytes\n," sizeof(long int));
+	printf("Size of a long long int : %lu bytes\n," sizeof(long long int));
+	printf("Size of a float : %lu bytes\n," sizeof(float));
+	
+	return (0);
 }
