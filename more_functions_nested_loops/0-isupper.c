72da907eae95a5de05d16c4e1cb323f639746d95
Mathijs Dickson <mathijs.dickson@outlook.com>
2025-02-24 06:47:24 +0000

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ *
+ * Return: Always 0
+ *
+ * @c; c character to be checked
+ */
+
+
+
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+		return (1);
+
+
+	return (0);
+}
