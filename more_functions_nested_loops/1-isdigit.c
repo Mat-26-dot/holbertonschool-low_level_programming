92a491a4b64f2ae8c857f4b33624a19e8762e101
Mathijs Dickson <mathijs.dickson@outlook.com>
2025-02-24 09:45:28 +0000

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ *
+ * Return: Always 0 
+ *
+ * @c: c character to be checked
+ */
+
+
+int  _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+		return (1);
+
+
+
+	return (0);
+}
