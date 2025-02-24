6f5dbbcce2fd6c243a43d4a48499aa82fbbbb4df
Mathijs Dickson <mathijs.dickson@outlook.com>
2025-02-24 21:12:33 +0000

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * mul(int a, int b) - Entry point
+ *
+ *Return: Always 0
+ *
+ * @mul(int a, int b) to be checked
+ */
+
+
+int mul (int a, int b)
+{	
+	
+	int result = a * b;
+
+
+
+
+
+	return result;
+}
