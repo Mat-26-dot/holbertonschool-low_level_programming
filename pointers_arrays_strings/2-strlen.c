f76d2ac2b73869e6ff1725a5a6d873e26896564f
Mathijs Dickson <mathijs.dickson@outlook.com>
2025-02-26 23:08:46 +0000

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -4,9 +4,6 @@
  *_ strlen - Returns the lenghth of a string
  *@s: Pointer to the string
  *
- * This function countsthe number of characters in the string
- * until it reaches the null terminator ('\0')
- *
  * Return: The length of the string
  */
 int _strlen(char *s)
