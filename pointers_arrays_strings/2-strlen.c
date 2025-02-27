fb1d2fce5930b2df6c657a94a34cf2db4257cc4e
Mathijs Dickson <mathijs.dickson@outlook.com>
2025-02-27 02:42:02 +0000

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -4,10 +4,10 @@
  * _strlen - Returns the length of a string.
  * @s: The string whose length is to be measured.
  *
- * Description: This function iterates through the string 
+ * Description: This function iterates through the string
  *              until the null terminator is found.
- * 
- * Return: The length of the string (excluding the null character).
+ *
+ * Return: The length of the string (excluding the null character)
  **/
 int _strlen(char *s)
 {
