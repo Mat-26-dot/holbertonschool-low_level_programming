a90630cfa2e44c1150177f98dc0477e60bd28f69
Mathijs Dickson <mathijs.dickson@outlook.com>
2025-02-27 02:34:14 +0000

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -3,9 +3,9 @@
 /**
  *_ strlen - Returns the lenghth of a string
  *
- *@s: Pointer to the string
+ * @s: Pointer to the string whose length is to be determined
  *
- * Return: The length of the string
+ * Return: The length of the string (number of characters before '\0')
  */
 int _strlen(char *s)
 {
