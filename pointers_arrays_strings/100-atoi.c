da291a22de2a68695297603e0634f2ee42fd751b
Mat Dickson <mathijs.dickson@outlook.com>
2025-02-28 01:39:32 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,8 +11,8 @@ int _atoi(char *s)
 	int i = 0, sign = 1, result = 0;
 
 
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9')
-		&& s[i] != '+' && s[i] != '-')
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9') && 
+		s[i] != '+' && s[i] != '-')
 	{
 		i++;
 	}
