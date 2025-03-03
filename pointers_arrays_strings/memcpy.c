941f6d76c09be353b1087f08304774b74eb8ddd4
Mat-dot-26 <mat.outlook.com>
2025-03-03 04:30:33 +0000

    1-memcpy.c

---
+++ pointers_arrays_strings/memcpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Copies n bytes from memory area src to memory area dest.
+ * @n: Number of bytes to copy
+ * @src: Source memory area
+ * @dest: Destination memory area
+ *Return: original_dest.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	char *original_dest = dest;
+
+	for (i = 0; i < n; i++;)
+	{
+		dest[i] = src[i];
+
+	}
+
+		return (original_dest);
+
+}
+	
+
+	
+
+
