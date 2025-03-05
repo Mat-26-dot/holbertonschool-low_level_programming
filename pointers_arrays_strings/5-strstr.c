91fbc47e83fb83ecb0ffe3e5a19429e1edcd29a8
mat-dot-26 <matd@outlook.com>
2025-03-05 04:31:00 +0000

    5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -18,18 +18,25 @@ char *_strstr(char *haystack, char *needle)
 	if (needle[0] == '\0')
 		return (haystack);
 
+	i = 0;
 	while (haystack[i] != '\0')
 	{
-		j = 0;
-		while (needle[j] != '\0' && haystack[i + j] == needle[j])
+		if (haystack[i] == needle[0])
 		{
+			j = 0;
+			while (needle[j] != '\0')
+			{
+
+				if (haystack[i + j]
+
+		!= needle[j])
+				break;
 			j++;
 		}
-
-			if (needle[j] == '\0')
-				return (&haystack[i]);
-
-		i++;
+		if (needle[j] == '\0')
+			return (&haystack[i]);
 	}
-		return (NULL);
+	i++;
+	}
+	return (NULL);
 }
