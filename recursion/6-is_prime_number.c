15b76b4d104d2f4a51aff5d0ab722ac25045b20d
mat <mat@gmail.com>
2025-03-08 04:23:43 +0000

    6-is_prime_number.c

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+int check_prime(int n, int divisor);
+
+/**
+ * is_prime_number - Checks if a number is prime
+ * @n: The number to be checked
+ *
+ * Return: 1 if the number is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	return (check_prime(n, 2));
+}
+
+/**
+ * check_prime - Recursively checks if a number is prime
+ * @n: The number to be checked
+ * @divisor: The current divisor being checked
+ *
+ * Return: 1 if the number is prime, 0 otherwise
+ */
+int check_prime(int n, int divisor)
+{
+	if (divisor * divisor > n)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (check_prime(n, divisor + 1));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -8,7 +8,8 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt_helper(int n, int i);
-
+int check_prime(int n, int divisor);
+int is_prime_number(int n);
 
 
 
