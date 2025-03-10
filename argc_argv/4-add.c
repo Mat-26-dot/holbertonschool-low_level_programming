0eb9150357b1961ff42828ccc2da4365310ad665
md <mat@outlook.com>
2025-03-10 04:23:30 +0000

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -27,14 +27,14 @@ int main(int argc, char *argv[])
 
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-	
+
 			if (!isdigit(argv[i][j]))
 			{
-		
+
 				printf("Error\n");
-		
+
 				return (1);
-			}	
+			}
 
 		}
 			sum += atoi(argv[i]);
