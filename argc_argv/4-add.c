f7c4b15d87d7e15bb67d9e00a62edd85772e9b12
md <mat@outlook.com>
2025-03-10 04:21:45 +0000

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - Entry point
+ * main - Adds positive numbers passed as command-line arguments.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of strings containing the arguments.
  *
- * Return: Always 0 (Success)
+ * Return: 0 if successful, 1 if any argument contains non-digits.
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	
+
+	int i, j, sum = 0;
+
+
 	if (argc == 1)
-	{	printf("0\n");
-		
+	{
+		printf("0\n");
+
 		return (0);
 	}
+
 	for (i = 1; i < argc; i++)
 	{
-		printf("Argument %d: %s\n", i, argv[i]);
 
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+	
+			if (!isdigit(argv[i][j]))
+			{
+		
+				printf("Error\n");
+		
+				return (1);
+			}	
+
+		}
+			sum += atoi(argv[i]);
 	}
+
+		printf("%d\n", sum);
+
 		return (0);
 }
