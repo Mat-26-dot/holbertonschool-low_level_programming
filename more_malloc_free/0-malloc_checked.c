7dec80a7de06fcf1feb3e944f14aa09369df98ce
md <md@outlook.com>
2025-03-14 03:52:44 +0000

    0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - Allocates memory of size b bytes
+ * @b: bytes of memory
+ * Return: checked
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *checked;
+	
+	checked = malloc(b);
+
+	if (checked == NULL)
+		exit(98);
+
+	return (checked);
+}
