0ed3ebfef9cfe34b9707ff504404951e68b7ba7d
md <md@outlook.com>
2025-03-19 04:57:02 +0000

    2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,14 +11,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;	
+	int i;
 
-	if (size <= 0)
+	if (array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		cmp(array[i]);
+
+	if (cmp(array[i]))
+		return (i);
 	}
-		return (*array);
+
+		return (-1);
 }
