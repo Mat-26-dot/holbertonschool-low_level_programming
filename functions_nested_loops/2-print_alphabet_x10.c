a1616fb2c8aeb46f0c907b95d2849680eb8e2eb1
md <md@outlook.com>
2025-03-29 01:10:33 +0000

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase 10 times
+ *
+ * Return: Always 0. (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i; /*outer loop counter*/
+	char c; /*inner loop counter*/
+
+	for (i = 0; i < 10; i++)/* outter loop prints alph x10*/
+	{
+		for (c = 'a'; c <= 'z'; c++)/* inner loop iterates a to z*/
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
