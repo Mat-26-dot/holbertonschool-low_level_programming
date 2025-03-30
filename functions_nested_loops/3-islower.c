d8a37ba450850f7902714a500974b4b7b7e9b041
md <md @outlook.com>
2025-03-30 06:26:33 +0000

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - that checks for lower-`case letters
+ * @c: character to check
+ * Return: 1 if lowercase, 0 otherwise
+ */
+int _islower(int c)
+{	
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1); /*Return 1 if c is lowercase*/
+	}
+	else
+	{
+		return (0); /*Return 0 otherwise*/
+	}
+}
