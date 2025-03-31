f043aa068fc4b7e849a23833c688db9cc03a698b
md <md@outlook.com>
2025-03-31 22:37:14 +0000

    0-print_list.c

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -7,10 +7,12 @@
  * @len: length of the string
  * @next: points to the next node
  */
-typedef struct list_s {
-    char *str;
-    unsigned int len;
-    struct list_s *next; /* Correct reference using struct tag */
-} list_t;
+typedef struct list_s 
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next; /* Correct reference using struct tag */
+	}list_t;
+
 
 #endif
--- singly_linked_lists/main.h
+++ singly_linked_lists/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-size_t print_list(const list_t *h);
+
 #include "lists.h"
+size_t print_list(const list_t *h);
+
 
 #endif
 
