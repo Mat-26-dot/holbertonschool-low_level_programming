37e510e5553052b6f6747885847bbd21c50b4005
md <md@outlook.com>
2025-03-31 23:29:52 +0000

    0-print_list.c

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -7,12 +7,12 @@
  * @len: length of the string
  * @next: points to the next node
  */
-typedef struct list_s 
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next; /* Correct reference using struct tag */
-	}list_t;
+	} list_t;
 
 size_t print_list(const list_t *h);
 
