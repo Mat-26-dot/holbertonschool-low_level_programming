b9a664ee29f511605ffa865b62e80eff77750abe
md <md@outlook.com>
2025-04-01 22:41:35 +0000

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,27 +11,27 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node; /*Declare *new_node at the top*/
 
-	list_t *new_node = malloc(sizeof(list_t)); /* Allocate memory for new node */
+	if (str == NULL) /* Step 1 - Parameter validation */
+		return (NULL);
 
+	new_node = malloc(sizeof(list_t)); /* 2. Malloc mem for new node */
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str); /* Duplicate str using strdup */
+	new_node->str = strdup(str); /* Step 3. Duplicate str using strdup */
 	if (new_node->str == NULL)
 	{
-
 		free(new_node); /* Free allocated memory for the node */
 			return (NULL);
 
-		new_node->len = strlen(new_node->str); /*Store len of str */
-		new_node->next = NULL; /* Initialize the next pointer to NULL*/
+		new_node->len = strlen(new_node->str); /* Step 4. Store string length */
+		new_node->next = *head;  /* Point new node's next to current head */
 
-		new_node->next = *head;  /* Point the new node's next to the current head */
-
-		*head = new_node; /* Update the head pointer to point to the new node */
+		*head = new_node; /* Step 5. Update the head pointer to point to new node */
 		}
-			return (new_node); /* Return the new node as confirmation of success */
+			return (new_node); /* Return new node as confirmation of success */
 
 
 }
