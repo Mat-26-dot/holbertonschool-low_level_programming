0f1043a4fe3247ffd97aa6feebd5bf6bc9b6e279
md <md@outlook.com>
2025-04-01 21:52:23 +0000

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -17,19 +17,21 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 
-	new mode->str == strdup(str); /* Duplicate str using strdup */
+	new_node->str = strdup(str); /* Duplicate str using strdup */
 	if (new_node->str == NULL)
 	{
+
 		free(new_node); /* Free allocated memory for the node */
 			return (NULL);
 
-	new_node->len = strlen(new_node->str); /*Store len of str */
-	new_node->next = NULL; /* Initialize the next pointer to NULL*/
+		new_node->len = strlen(new_node->str); /*Store len of str */
+		new_node->next = NULL; /* Initialize the next pointer to NULL*/
 
-	new_node->next = *head;  /* Point the new node's next to the current head */
+		new_node->next = *head;  /* Point the new node's next to the current head */
 
-	*head = new_node; /* Update the head pointer to point to the new node */
+		*head = new_node; /* Update the head pointer to point to the new node */
+		}
+			return (new_node); /* Return the new node as confirmation of success */
 
-	return (new_node); /* Return the new node as confirmation of success */
-	}
+
 }
